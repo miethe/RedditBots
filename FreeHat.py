69869da7d1c9349d5ac1b626e235de30e02f2ba2
Nick Miethe <6800980+miethe@users.noreply.github.com>
2018-01-29 09:14:37 -0500

    Create FreeHat.py

---
+++ FreeHat.py
@@ -0,0 +1,40 @@
+####################
+# Nick Miethe
+# 4/20/2017
+####################
+
+#!/usr/bin/python
+import praw
+import time
+import random
+
+def upvote(redditor):
+	#comments_new = redditor.comments(limit=None)
+	for comment in redditor.comments.new():
+		comment.upvote()
+			
+def main():
+
+	#reddit = praw.Reddit('bot2')
+	reddit = praw.Reddit(client_id='####',
+                     client_secret='####',
+                     user_agent='####',
+                     username='####',
+                     password='####')
+	
+	while True:
+		user = reddit.redditor('NessieB0t')
+		upvote(user)
+		count = random.randint(1,10)
+		while count > 0:
+			subreddit = reddit.subreddit('random')
+			for submission in subreddit.new():
+				for comment in submission.comments.list():
+					comment.upvote()
+				submission.reply("This thread has been visited by the Free Hat Fairy!")
+				count -=1
+				break
+	
+	
+if __name__ == "__main__":
+    main()
